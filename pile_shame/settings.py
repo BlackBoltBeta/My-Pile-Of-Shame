dd85cf94b7dccb44e6e9cf2eaee0de71731ea9ba
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-11-24 16:12:21 +0100

    Add:Procfile, deploy setup

--- pile_shame/settings.py
+++ pile_shame/settings.py
@@ -4,6 +4,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR =  os.path.join(BASE_DIR, 'templates')
 
 
 if os.path.exists('env.py'):
@@ -17,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['my-pile-of-shame.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -28,7 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'hobbycentre',
 ]
 
@@ -47,7 +50,7 @@ ROOT_URLCONF = 'pile_shame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'Cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
